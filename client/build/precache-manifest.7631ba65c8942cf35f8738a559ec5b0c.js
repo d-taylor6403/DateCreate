74f27e93e54725785fc91e760c223c1239357c7d
d-taylor6403 <dannielletaylor@outlook.com>
2020-08-11 21:55:02 -0500

    adding database credentials

---
+++ client/build/precache-manifest.7631ba65c8942cf35f8738a559ec5b0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7400a79dd084de31fe6",
+    "url": "/static/css/main.8cbd7071.chunk.css"
+  },
+  {
+    "revision": "d7400a79dd084de31fe6",
+    "url": "/static/js/main.632c3033.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "93539bfc294b4532ec28",
+    "url": "/static/js/2.85d0df2e.chunk.js"
+  },
+  {
+    "revision": "01b0b6782f92916b6b32b75f34de1116",
+    "url": "/static/media/DateCreateLogo.01b0b678.png"
+  },
+  {
+    "revision": "81cfcaa4920bfaa123b1c1557727f272",
+    "url": "/static/media/DateCreateBranding.81cfcaa4.png"
+  },
+  {
+    "revision": "43aab75d752e18f7f01c5ed59bb827a5",
+    "url": "/index.html"
+  }
+];
